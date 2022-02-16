1604f81781ee606c272e96163ade7eaf90e46fef
hedaecra <3883@holbertonschool.com>
2022-02-15 22:27:33 -0500

    Update Sort.h

--- sort.h
+++ sort.h
@@ -28,15 +28,17 @@ void swap_node(listint_t *node_x, listint_t *node_y, listint_t **head);
 void selection_sort(int *array, size_t size);
 void swap(int a, int b, int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void quick_part(int *array, int low, int high, size_t size);
+size_t lomuto(int *array, int low, int high, size_t size);
 /**
  * ss - void shell_sort(int *array, size_t size);
  * void cocktail_sort_list(listint_t **list);
- * void counting_sort(int *array, size_t size);
+ - void counting_sort(int *array, size_t size);
  * void merge_sort(int *array, size_t size);
  * void heap_sort(int *array, size_t size);
  * void radix_sort(int *array, size_t size);
- * void bitonic_sort(int *array, size_t size);
- * void quick_sort_hoare(int *array, size_t size);
+ - void bitonic_sort(int *array, size_t size);
+ - void quick_sort_hoare(int *array, size_t size);
  * void sort_deck(deck_node_t **deck);
  */
 
